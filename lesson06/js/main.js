51e0741da30d29cfccdcb37612191b9bdd8391ad
SebastianDimun <88368807+SebastianDimun@users.noreply.github.com>
2021-08-13 14:35:45 +0200

    init commit

---
+++ lesson06/js/main.js
@@ -0,0 +1,20 @@
+function sum(a,b){
+    return a+b;
+}
+
+let sucet = sum(2,6);
+
+while(sucet < 10){
+    console.log('malo')
+    sucet++ 
+}
+
+if (sucet>10){
+    console.log('dost vela')
+}else{
+    console.log('malo fess');
+}
+
+const box = document.querySelector('#box');
+// box.style.width='500px'
+console.dir(box);
