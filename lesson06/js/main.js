ff292ffc5557d93f1eaf93235c061ab5f665557e
danielchovanec <Daniel.Chovanec@ness.com>
2021-08-16 13:04:09 +0200

    lesson6

---
+++ lesson06/js/main.js
@@ -0,0 +1,52 @@
+// let krabicka = document.querySelector('#box');
+// krabicka.style.width = '50px';
+// console.log(krabicka);
+
+// var width
+// var width = screen.width;
+// var height = screen.height;
+
+// console.log(width, height)
+// let boxMovement = setTimeout(enlargen(),100)
+
+function enlargen(){
+    var box = document.getElementById("box1");
+    var currentWidthRed = box.clientWidth;
+    var currentHeightRed = box.clientHeight;
+    if(currentWidthRed == screen.width){
+        alert("Max width reached.");
+    } else if(currentHeightRed == screen.height){
+        alert("Max height reached")
+    } else {
+        box.style.width = (currentWidthRed + 3) + "px";
+        box.style.height = (currentHeightRed + 3) + "px";
+    } 
+}
+
+var boxOffset = 0;
+function move() {
+    boxOffset += 30
+    document.getElementById("box2").style.left = boxOffset + "px";
+  }
+
+// while(boxOffset <= 500){
+//     move();
+// }
+
+
+// function sum(a,b) {
+//     return a + b
+// }
+
+// let sucet = sum (2,4);
+
+// while (sucet < 10) {
+//     console.log('malo')
+//     sucet = sucet + 1
+// }
+// if (sucet > 10){
+//     console.log('dost vela')
+// } else {
+//     console.log('malo')
+// }
+
