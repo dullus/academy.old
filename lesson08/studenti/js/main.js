d2025c77e5bd4770a6bfee5e77fb0093c9aaa394
Dalibor Schön <dalibor.schon@ness.com>
2021-08-18 15:35:03 +0200

    lesson08 final

--- lesson08/studenti/js/main.js
+++ lesson08/studenti/js/main.js
@@ -1,2 +1,55 @@
 const rootElem = document.getElementsByTagName('html').item(0);
 const themeElem = document.getElementsByName('theme').item(0);
+const formElem = document.getElementsByClassName('myForm')[0];
+
+themeElem.addEventListener('change', function(ev) {
+  const elem = ev.target;
+  const theme = elem.value;
+  switchTheme(theme);
+});
+
+function switchTheme(theme) {
+  rootElem.setAttribute('data-theme', theme);
+}
+
+formElem.addEventListener('submit', (ev) => {
+  ev.preventDefault();
+  clearMessage();
+  let error = false;
+
+  if (formElem['firstName'].value.trim() === '') {
+    error = true;
+    showMessage('alert', 'Please fill "First Name".');
+  }
+  if (formElem['surName'].value.trim() === '') {
+    error = true;
+    showMessage('alert', 'Please fill "Surname".');
+  }
+
+  /*
+  if (!error) {
+  if (error !== true) {
+  if (error === false) {
+  */
+  if (!error) {
+    const full = fullName(
+      formElem['firstName'].value,
+      formElem['surName'].value
+    );
+    showMessage('success', full);
+  }
+});
+
+function showMessage(type, txt) {
+  const el = document.getElementById(type);
+  el.style.display = 'block';
+  el.innerText = el.innerText + ' ' + txt;
+}
+
+function clearMessage() {
+  const elems = document.getElementsByClassName('alert');
+  for (el of elems) {
+    el.style.display = 'none';
+    el.innerText = '';
+  }
+}
