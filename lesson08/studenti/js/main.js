28c22da54c4a1d172cf3b5336b20c0caccd7481b
danielchovanec <Daniel.Chovanec@ness.com>
2021-08-20 09:03:59 +0200

    lesson8

--- lesson08/studenti/js/main.js
+++ lesson08/studenti/js/main.js
@@ -1,2 +1,52 @@
 const rootElem = document.getElementsByTagName('html').item(0);
 const themeElem = document.getElementsByName('theme').item(0);
+const formElem = document.getElementsByClassName('myForm')[0];
+
+themeElem.addEventListener('change', function(ev) {
+    console.log(ev);
+    const elem = ev.target;
+    const theme = elem.value;
+
+switchTheme(theme)});
+
+function switchTheme(theme){
+    rootElem.setAttribute('data-theme', theme);
+}
+
+formElem.addEventListener('submit', (ev) => {     //arrow function, ktora bude vediet aj o veciach mimo nej..o zvysku sveta
+    ev.preventDefault();
+    clearMessage();
+    let error = false;
+
+    if (formElem['firstName'].value === ''){
+        error = true;
+        showMessage('alert', 'Please fill in your First Name.')
+    }
+    if (formElem['surName'].value === ''){
+        error = true;
+        showMessage('alert', 'Please fill in your Surname.')
+    }
+
+    if(!error) {
+    const full = fullName(
+        formElem['firstName'].value,
+        formElem['surName'].value
+        );
+        showMessage('success', full);
+        document.getElementById('success').innerText = full;
+    }
+});
+
+function showMessage(type, txt) {
+    const el = document.getElementById(type);
+    el.style.display = 'block';
+    el.innerText = el.innerText + ' ' + txt;
+}
+
+function clearMessage(){
+    const elems = document.getElementsByClassName('alert');
+    for (el of elems){
+        el.style.display = 'none';
+        el.innerText = '';
+    }
+}
--- lesson08/studenti/js/utils.js
+++ lesson08/studenti/js/utils.js
@@ -1,5 +1,21 @@
 function fullName(first, last) {
+    return capitalize(first) + ' ' + capitalize(last);
 }
 
 function capitalize(txt) {
+    if(txt === '' || txt === undefined || txt === null)
+    if (txt){
+    return txt[0].toUpperCase() + txt.substring(1);
+    }
+    return '';
+}
+
+function capitalizeSentence(txt) {
+    const words = txt.trim().split(' ');
+    let out = capitalize(words[0]);
+
+    for (i=1; i<words.length; i++) {
+            out = out + " " + capitalize(words[i]);
+    }
+    return out;
 }
