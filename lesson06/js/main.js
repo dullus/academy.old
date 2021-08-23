478974581d79e3ddda189dae5b4d1b17a857c4bc
danielchovanec <Daniel.Chovanec@ness.com>
2021-08-23 13:35:29 +0200

    kocka posledne upravy

--- lesson06/js/main.js
+++ lesson06/js/main.js
@@ -1,56 +1,44 @@
-// let krabicka = document.querySelector('#box');
-// krabicka.style.width = '50px';
-// console.log(krabicka);
+let box = document.getElementById("box1");
 
-// var width
-// var width = screen.width;
-// var height = screen.height;
-
-// console.log(width, height)
-// let boxMovement = setTimeout(enlargen(),100)
-
-function enlargen(){
-    var box = document.getElementById("box1");
-    var currentWidthRed = box.clientWidth;
-    var currentHeightRed = box.clientHeight;
-    if(currentWidthRed == screen.width){
-        alert("Max width reached.");
-    } else if(currentHeightRed == screen.height){
-        alert("Max height reached")
-    } else {
+function enlarge(){
+    // if(currentWidthRed == screen.width){
+    //     // alert("Max width reached.");
+    // } else if(currentHeightRed == screen.height){
+    //     alert("Max height reached")
+    // } else {
+        let currentWidthRed = box.clientWidth;
+        let currentHeightRed = box.clientHeight;
         box.style.width = (currentWidthRed + 1) + "px";
         box.style.height = (currentHeightRed + 1) + "px";
+        
+        return;
     } 
+// }
+
+function shrink(){
+    let currentWidthRed = box.clientWidth;
+    let currentHeightRed = box.clientHeight;
+    box.style.width = (currentWidthRed - 1) - "px";
+    box.style.height = (currentHeightRed - 1) - "px";
 }
 
-var boxOffset = 0;
-function move() {
-    boxOffset += 5
-    document.getElementById("box2").style.left = boxOffset + "px";
-  }
+
+// var boxOffset = 0;
+// function move() {
+//     boxOffset += 5
+//     document.getElementById("box2").style.left = boxOffset + "px";
+    // if(currentWidthBlue == screen.width){
+    //     document.getElementById("box2").style.left = boxOffset + "px";
+    // }
+//   }
 
 
-setInterval(move, 10)
-setInterval(enlargen, 10)
+// setInterval(move, 10)
+setInterval(enlarge, 10);
+clearInterval(enlarge);
 
-// while(boxOffset <= 500){
-//     move();
-// }
+let btn = document.getElementById("button")
 
-
-// function sum(a,b) {
-//     return a + b
-// }
-
-// let sucet = sum (2,4);
-
-// while (sucet < 10) {
-//     console.log('malo')
-//     sucet = sucet + 1
-// }
-// if (sucet > 10){
-//     console.log('dost vela')
-// } else {
-//     console.log('malo')
-// }
+btn.addEventListener("click", shrink)
+setInterval(shrink, 10)
 
