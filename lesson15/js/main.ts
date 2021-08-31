5428d8d40de1a6c27997797d852a9ab05b1e1d83
kristinapuskasova <Kristina.Puskasova@ness.com>
2021-08-31 15:01:41 +0200

    lesson15

---
+++ lesson15/js/main.ts
@@ -0,0 +1,14 @@
+import { someFun } from "./util.js"
+
+console.log(someFun('ahoj',1));
+
+let xx:any;
+let aa: string | number | boolean;
+
+aa = '4'
+aa = 4
+aa = true
+
+let bb: 'one' | 'two';
+
+bb = 'two'
