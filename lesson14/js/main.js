6a4a381e302f8ae14d6b4d740f20423b7bdf593e
danielchovanec <Daniel.Chovanec@ness.com>
2021-08-31 12:30:30 +0200

    checkbox alert done

--- lesson14/js/main.js
+++ lesson14/js/main.js
@@ -1,35 +1,66 @@
-fetch('mocks/todo.json')
+fetch('https://jsonplaceholder.typicode.com/todos')
   .then(response => response.json())
   .then(data => {
-     console.log(data)
+    appendData(data);
+    // console.log(data)
   });
 
-function fakeAsync() {
-    return new Promise((resolve, reject) => {
-        window.setTimeout(() => {
-            const fortune = Math.floor(Math.random() * 2);
+function appendData(data){
+for (let i = 0; i < data.length; i++){
+  const mainPanel = document.getElementById("mainPanel");
+  const checkbox = document.createElement('Input');
 
-            if (fortune == 0) {
-                resolve('ok')
-            } else {
-                reject('bad')
-            }
-        }, 1000);
+    var div = document.createElement("div");
+    div.innerHTML = 'Id: ' + data[i].id;
+    checkbox.addEventListener('change', () => {
+    alert(data[i].title)
     })
-}
+    mainPanel.appendChild(div);
+    checkbox.type = 'checkbox';
+    mainPanel.appendChild(checkbox);
+    }
+    
+  }
 
-fakeAsync()
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
 
-function renderData(title) {
-    const titleElement = createTodoTitle(title)
-    document.getElementById('todoTitle').appendChild(titleElement)
-}
 
-function createTodoTitle(title) {
-    const titleElement = document.createElement('p')
-    titleElement.innerHTML = title
 
-    return titleElement;
-}
+
+
+
+
+
+
+
+
+
+
+// function fakeAsync() {
+//     return new Promise((resolve, reject) => {
+//         window.setTimeout(() => {
+//             const fortune = Math.floor(Math.random() * 2);
+
+//             if (fortune == 0) {
+//                 resolve('ok')
+//             } else {
+//                 reject('bad')
+//             }
+//         }, 1000);
+//     })
+// }
+
+// fakeAsync()
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+// function renderData(title) {
+//     const titleElement = createTodoTitle(title)
+//     document.getElementById('mainPanel').appendChild(titleElement)
+// }
+
+// function createTodoTitle(title) {
+//     const titleElement = document.createElement('p')
+//     titleElement.innerHTML = title
+
+//     return titleElement;
+// }
