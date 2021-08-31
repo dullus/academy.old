3597a37d63d1d35c63e2383dc5c2640c7df13e47
stanohlivjak <88368171+stanohlivjak@users.noreply.github.com>
2021-08-31 13:30:43 +0200

    homework rework

--- lesson14/js/main.js
+++ lesson14/js/main.js
@@ -1,50 +1,37 @@
 fetch('https://jsonplaceholder.typicode.com/todos')
   .then(response => response.json())
   .then(data => {
-     const todocka = data.map(todo => createTodoTitle(todo.title));
-     console.log(todocka)
-    document.getElementById('todoTitle').append(...todocka)
-
+    const todocka = data.map(todo => createTodoTitle(todo.title, todo.id));
+    document.getElementById('todoTitle').append(...todocka);
   });
 
-function renderData(title) {
-    const titleElement = createTodoTitle(title)
-    document.getElementById('todoTitle').appendChild(titleElement)
+function createTodoTitle(title, id) {
+  const titleElement = document.createElement('div');
+  const inputElement = document.createElement('input');
+  const p = document.createElement("p");
+  titleElement.style.display = 'flex';
+  titleElement.style.justifyContent = 'center';
+  titleElement.style.alignItems = 'center';
+  inputElement.setAttribute("type", "checkbox");
+  inputElement.setAttribute("class", "input");
+  inputElement.setAttribute("id", `ID: ${id}`);
+
+  //addEventListener change logic
+  // inputElement.addEventListener("change", (e) => {
+  // windowAlert(e.target)
+  // });
+
+  // addEventListener click logic
+  inputElement.addEventListener('click', () => windowAlert(inputElement));
+
+  p.innerHTML = title;
+  titleElement.appendChild(inputElement);
+  titleElement.appendChild(p);
+  return titleElement;
 }
 
-function createTodoTitle(title) {
-    const titleElement = document.createElement('p')
-    titleElement.innerHTML = title
-
-    return titleElement;
+function windowAlert(input) {
+  if (input.checked) {
+    window.alert(input.id);
+  }
 }
-
-/ * part 2 */
-
-fetch('mocks/todo.json')
-  .then(response => response.json())
-  .then(data => {
-     console.log(data)
-  });
-
-function fakeAsync() {
-  return new Promise(( resolve, reject ) => {
-    window.setTimeout(()=>{
-      const fortune = Math.floor(Math.random() * 2);
-      if (fortune === 0) {
-        resolve('ok')
-      } else {
-        reject('bad');
-      }
-    }, 1000);
-  })
-}
-
-const fa = fakeAsync()
-  .then((data)=> console.log(data))
-  .catch((err) => console.log(err))
-  .finally(() => console.log(fa));
-
-console.log(fa);
-console.log(2);
-console.log(3);
