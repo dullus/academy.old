823a0c694c010732b19430cbb92e6ce4b60cd3f7
PavelPrcin <Pavol.Prcin@ness.com>
2021-08-31 13:31:21 +0200

    homework - fetch

--- lesson14/js/main.js
+++ lesson14/js/main.js
@@ -4,22 +4,17 @@ fetch("https://jsonplaceholder.typicode.com/todos")
     const todos = data
       .slice(0, 5)
       .map((todo) => createTodoTitle(todo.title, todo.id));
+
     document.getElementById("todoTitle").append(...todos);
 
-    var list = document.getElementsByClassName("checkbox");
+    let list = document.getElementsByClassName("checkbox");
     for (let i = 0; i < list.length; i++) {
       list[i].addEventListener("change", () => {
         alert(list[i].id);
       });
     }
     console.log(list);
-    // console.log(todos);
   });
-
-function renderData(title) {
-  const titleElement = createTodoTitle(title);
-  document.getElementById("todoTitle").appendChild(titleElement);
-}
 
 function createTodoTitle(title, id) {
   const titleDiv = document.createElement("div");
