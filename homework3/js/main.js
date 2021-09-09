8d9a025235148ca0b36db5831992cfde36c67b1c
KristianKovacs <Kristian.Kovacs@ness.com>
2021-09-09 12:54:47 +0200

    SpotifyJS part 2 - adding specific data about artist with id:1 in top section

--- homework3/js/main.js
+++ homework3/js/main.js
@@ -1,5 +1,62 @@
-fetch('https://613622df8700c50017ef5455.mockapi.io/api/v1/artist/1')
-  .then(response => response.json())
+async function readAPI() {
+
+  const urlAPI = 'https://613622df8700c50017ef5455.mockapi.io/api/v1/artist/1';
+
+  const response = await fetch(urlAPI)
+
+  // console.log(response);
+
+  if(!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+
+  return await response.json(); 
+}
+
+function manipulateDOM() {
+  readAPI()
   .then(data => {
-    console.log(data)
-})
+    console.log(data);
+    
+    const bandName = document.getElementById("band-name")
+    bandName.innerHTML = ` ${data.name} ${data.lastName} `;
+
+    const latestReleaseName= document.getElementById("latest_release_name");
+    latestReleaseName .innerHTML = `${data.music.latest.name}`;
+
+    const latestReleaseImage = document.getElementById("latest_release_image");
+    latestReleaseImage .setAttribute("src", `${data.music.latest.image}`); 
+
+  const albumReleaseDate = document.getElementById('latest_relase_date');
+  const x = new Date(Date.parse(data.music.latest.releaseDate)).toLocaleDateString('en-US', {month:'short'});
+  const y = new Date(Date.parse(data.music.latest.releaseDate)).toLocaleDateString('en-US', {day: 'numeric'});
+  const z = new Date(Date.parse(data.music.latest.releaseDate)).toLocaleDateString('en-US', {year: 'numeric'}); 
+  
+  albumReleaseDate.innerHTML = x + " " + y + ", " + z;
+    
+
+  })
+}
+
+manipulateDOM();
+
+function manipulateDate() {
+  const albumReleaseDate = document.getElementById('latest_relase_date');
+  const x = new Date(Date.parse(data.music.latest.releaseDate)).toLocaleDateString('en-US', {month:'short'});
+
+}
+
+
+// const albumReleaseDate = document.getElementById('date')
+//     const x = new Date(Date.parse(data.music.latest.releaseDate)).toLocaleDateString('en-US', {month:'short'});
+//     const y = new Date(Date.parse(data.music.latest.releaseDate)).toLocaleDateString('en-US', {day: 'numeric'});
+//     const z = new Date(Date.parse(data.music.latest.releaseDate)).toLocaleDateString('en-US', {year: 'numeric'}); 
+    
+//     albumReleaseDate.innerHTML = x + " " + y + ", " + z;
+
+
+
+
+
+ 
