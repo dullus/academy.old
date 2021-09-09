d890ed0f05eb26f934659441a15564cd05d65017
ErikScecina <88367804+ErikScecina@users.noreply.github.com>
2021-09-09 11:12:06 +0200

    SpotifyJS part 1 - done

--- Spotify_Erik/js/main.js
+++ Spotify_Erik/js/main.js
@@ -8,5 +8,18 @@ function loadData(data) {
     
     const heading = document.getElementById('name');
     heading.innerHTML = data.name + " " + data.lastName;
+
+    const albumName = document.getElementById('song_name');
+    albumName.innerHTML = data.music.latest.name;
+ 
+    const albumReleaseDate = document.getElementById('date')
+    const x = new Date(Date.parse(data.music.latest.releaseDate)).toLocaleDateString('en-US', {month:'short'});
+    const y = new Date(Date.parse(data.music.latest.releaseDate)).toLocaleDateString('en-US', {day: 'numeric'});
+    const z = new Date(Date.parse(data.music.latest.releaseDate)).toLocaleDateString('en-US', {year: 'numeric'}); 
+    
+    albumReleaseDate.innerHTML = x + " " + y + ", " + z;
+
+    const albumImage = document.getElementById('latest_img')
+    albumImage.setAttribute('src', data.music.latest.image)
  
 }
