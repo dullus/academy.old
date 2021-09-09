77c08970f0500fed5c7d91471bf0b14ae834f612
ErikScecina <88367804+ErikScecina@users.noreply.github.com>
2021-09-09 10:45:24 +0200

    Spotify JS part1 - modified html/css 1/2

---
+++ Spotify_Erik/js/main.js
@@ -0,0 +1,13 @@
+fetch('https://613622df8700c50017ef5455.mockapi.io/api/v1/artist/1')
+.then(response => response.json())
+.then(data => {
+    loadData(data);
+})
+ 
+function loadData(data) {
+    
+ 
+    const heading = document.getElementById('name');
+    heading.innerHTML = data.name + " " + data.lastName;
+ 
+}
