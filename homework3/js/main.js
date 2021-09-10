c9e0087686ca169ac1413f79416e0b519f49f49b
KristianKovacs <Kristian.Kovacs@ness.com>
2021-09-10 11:10:16 +0200

    SpotifyJS part2 - create function for getting all from multiple artist

--- homework3/js/main.js
+++ homework3/js/main.js
@@ -1,61 +1,69 @@
-async function readAPI() {
+const  urlAPI = "https://613622df8700c50017ef5455.mockapi.io/api/v1/artist/1";
 
-  const urlAPI = 'https://613622df8700c50017ef5455.mockapi.io/api/v1/artist/1';
+const urlAPIs = ["https://613622df8700c50017ef5455.mockapi.io/api/v1/artist/1", "https://613622df8700c50017ef5455.mockapi.io/api/v1/artist/2", "https://613622df8700c50017ef5455.mockapi.io/api/v1/artist/3"];
 
-  const response = await fetch(urlAPI)
 
-  // console.log(response);
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+async function readAPI(resource) {
+
+  const response = await fetch(resource);
+
+  const data = await response.json();
 
   if(!response.ok) {
     const message = `An error has occured: ${response.status}`;
     throw new Error(message);
   }
-
-  return await response.json(); 
+  return data;
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+async function readAllAPI(resource) {
+let response, data;
+const dbAPI = [];
 
-function manipulateDOM() {
-  readAPI()
-  .then(data => {
-    console.log(data);
+for (const api of urlAPIs) {
+        response = await fetch(api);
+        data = await response.json();
+        
+        if(!response.ok) {
+          let message = `An error has occured: ${response.status}`;
+          throw new Error(message);
+        } 
+        dbAPI.push(data);
+}     
 
-  
-    
+return dbAPI;
+}
+ /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+
+async function manipulateDOM() {
+  const data = await readAllAPI(urlAPIs);
+
+  console.log(data.length);
+  let index = Math.floor(Math.random()*data.length) + 1;
+  console.log(index);
+
     const bandName = document.getElementById("band-name")
-    bandName.innerHTML = ` ${data.name} ${data.lastName} `;
+    bandName.innerHTML = ` ${data[index].name} ${data[index].lastName} `;
 
     const latestReleaseName= document.getElementById("latest_release_name");
-    latestReleaseName .innerHTML = `${data.music.latest.name}`;
+    latestReleaseName .innerHTML = `${data[index].music.latest.name}`;
 
     const latestReleaseImage = document.getElementById("latest_release_image");
-    latestReleaseImage .setAttribute("src", `${data.music.latest.image}`); 
+    latestReleaseImage .setAttribute("src", `${data[index].music.latest.image}`); 
 
   const albumReleaseDate = document.getElementById('latest_release_date');
-  const x = new Date(Date.parse(data.music.latest.releaseDate)).toLocaleDateString('en-US', {month:'short'});
-  const y = new Date(Date.parse(data.music.latest.releaseDate)).toLocaleDateString('en-US', {day: 'numeric'});
-  const z = new Date(Date.parse(data.music.latest.releaseDate)).toLocaleDateString('en-US', {year: 'numeric'}); 
+  const x = new Date(Date.parse(data[index].music.latest.releaseDate)).toLocaleDateString('en-US', {month:'short'});
+  const y = new Date(Date.parse(data[index].music.latest.releaseDate)).toLocaleDateString('en-US', {day: 'numeric'});
+  const z = new Date(Date.parse(data[index].music.latest.releaseDate)).toLocaleDateString('en-US', {year: 'numeric'}); 
   
   albumReleaseDate.innerHTML = x + " " + y + ", " + z;
     
-
-  })
-}
-
+  }
 manipulateDOM();
 
-function manipulateDate() {
-  const albumReleaseDate = document.getElementById('latest_relase_date');
-  const x = new Date(Date.parse(data.music.latest.releaseDate)).toLocaleDateString('en-US', {month:'short'});
-
-}
 
 
-// const albumReleaseDate = document.getElementById('date')
-//     const x = new Date(Date.parse(data.music.latest.releaseDate)).toLocaleDateString('en-US', {month:'short'});
-//     const y = new Date(Date.parse(data.music.latest.releaseDate)).toLocaleDateString('en-US', {day: 'numeric'});
-//     const z = new Date(Date.parse(data.music.latest.releaseDate)).toLocaleDateString('en-US', {year: 'numeric'}); 
-    
-//     albumReleaseDate.innerHTML = x + " " + y + ", " + z;
 
 
 
