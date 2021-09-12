83c813d99f22bb69a00700620beb63d3056babe9
ErikScecina <88367804+ErikScecina@users.noreply.github.com>
2021-09-12 15:42:41 +0200

    SpotifyJS part 2 - load random artist data

--- Spotify_Erik/js/main.js
+++ Spotify_Erik/js/main.js
@@ -33,9 +33,32 @@ fetch('https://613622df8700c50017ef5455.mockapi.io/api/v1/artist/')
   .then(data => {
     const randomArtist = rngArtist(data);
     console.log(randomArtist);
-   
+    fetch(`https://613622df8700c50017ef5455.mockapi.io/api/v1/artist/${randomArtist}`)
+    .then(response => response.json())
+    .then(data => {
+        loadData(data);
+    })
     function rngArtist(data) {
         return Math.ceil(Math.random() * data.length)
     }
 })
 
+function loadData(data) {
+    
+    const heading = document.getElementById('name');
+    heading.innerHTML = data.name + " " + data.lastName;
+
+    const albumName = document.getElementById('song_name');
+    albumName.innerHTML = data.music.latest.name;
+ 
+    const albumReleaseDate = document.getElementById('date')
+    const x = new Date(Date.parse(data.music.latest.releaseDate)).toLocaleDateString('en-US', {month:'short'});
+    const y = new Date(Date.parse(data.music.latest.releaseDate)).toLocaleDateString('en-US', {day: 'numeric'});
+    const z = new Date(Date.parse(data.music.latest.releaseDate)).toLocaleDateString('en-US', {year: 'numeric'}); 
+    
+    albumReleaseDate.innerHTML = x + " " + y + ", " + z;
+
+    const albumImage = document.getElementById('latest_img')
+    albumImage.setAttribute('src', data.music.latest.image)
+ 
+}
